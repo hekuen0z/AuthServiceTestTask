a1d7ddf4bf9f95784c6f1872957680b2b66c0385
hekuen0z <kaptur.swdev@gmail.com>
2023-12-29 08:31:42 +0300

    Add JwtException resolver.

--- src/main/java/app/magicphoto/authservice/controller/error/AuthenticationErrorHandlingControllerAdvice.java
+++ src/main/java/app/magicphoto/authservice/controller/error/AuthenticationErrorHandlingControllerAdvice.java
@@ -1,6 +1,9 @@
 package app.magicphoto.authservice.controller.error;
 
 import app.magicphoto.authservice.error.security.AuthenticationErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.SignatureException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
@@ -16,11 +19,35 @@ public class AuthenticationErrorHandlingControllerAdvice {
 
     @ResponseBody
     @ExceptionHandler(AuthenticationException.class)
-    @Operation(summary = "Метод для отправки AuthenticationErrorResponse")
+    @Operation(summary = "Метод для обработки AuthenticationErrorResponse.")
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public AuthenticationErrorResponse onAuthenticationException(AuthenticationException e) {
         return new AuthenticationErrorResponse(
                 HttpStatus.UNAUTHORIZED.toString(), e.getMessage()
         );
+    }
+
+    @ResponseBody
+    @ExceptionHandler(JwtException.class)
+    @Operation(summary = "Метод для обработки JwtException.")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public AuthenticationErrorResponse onJwtValidationException(JwtException e) {
+        AuthenticationErrorResponse response = null;
+
+        if(e instanceof SignatureException) {
+            response = new AuthenticationErrorResponse(
+                    HttpStatus.UNAUTHORIZED.toString(), "The JWT Signature is invalid");
+        }
+        if(e instanceof ExpiredJwtException) {
+            response = new AuthenticationErrorResponse(
+                    HttpStatus.UNAUTHORIZED.toString(), "The JWT token has expired");
+        }
+
+        if(response == null) {
+            response = new AuthenticationErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                    "Unknown internal JWT service error.");
+        }
+
+        return response;
     }
 }
